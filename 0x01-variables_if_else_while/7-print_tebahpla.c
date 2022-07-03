bbe5f1d30fc36bde48e7689b31b2e734fc830f63
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-03 08:45:35 -0700

    Reverse Laphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints a-z in reverse
+ *
+ * letters - variable name for char type
+ *
+ * while - statement
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -5,3 +5,4 @@ this prints out lower and upper case alphabets followed by new line
 This prints the alphabet in lowercase, followed by a new line except 'q' and 'e'
 prints numbers on base ten
 print numbers with putchar
+prints reverse alphabeths
