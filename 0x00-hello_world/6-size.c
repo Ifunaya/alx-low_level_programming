147ae9508d6e69dbfcaf8bf33b9346a3a480bd5d
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-03 14:21:16 -0700

    Corrections trial

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,12 +13,12 @@ int main(void)
 	int ch = sizeof(char);
 	int int_type = sizeof(int);
 	int float_type = sizeof(float);
-	int long_int = sizeof(long);
-	int ll_int = sizeof(long long);
+	int long_int = sizeof(long int);
+	int ll_int = sizeof(long long int);
 		printf("Size of a char: %d byte(s)\n", ch);
-		printf("Size of a int: %d byte(s)\n", int_type);
-		printf("Size of a long: %i byte(s)\n", long_int);
-		printf("Size of a long long: %d byte(s)\n", ll_int);
+		printf("Size of an int: %d byte(s)\n", int_type);
+		printf("Size of a long int: %d byte(s)\n", long_int);
+		printf("Size of a long long int: %d byte(s)\n", ll_int);
 		printf("Size of a float: %d byte(s)\n", float_type);
 		return (0);
 }
