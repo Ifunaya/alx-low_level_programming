c91031117595496e099932741f7980b070ce4dda
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-06 15:06:28 -0700

    alphabets fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - prints out the alphabet in lower case
- *   * Return: 0
- *    */
+ * print_alphabet - prints out the alphabet in lower case
+ * Return: 0
+ */
 void print_alphabet(void)
 {
 		char ch = 'a';
