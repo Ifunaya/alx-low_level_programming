c41fea6255f078668e416479edb0c631678e508a
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-06 15:13:49 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet_x10 - Prints the alphabets in lowercase 10 times
+ *   * Return: 0
+ *    */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i <= 9)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
