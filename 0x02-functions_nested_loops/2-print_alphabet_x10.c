50dd1f728bdc76dda689dc7ed072357b63873626
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-06 15:16:32 -0700

    alphabetx10 fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - Prints the alphabets in lowercase 10 times
- *   * Return: 0
- *    */
+ *print_alphabet_x10 - Prints the alphabets in lowercase 10 times
+ * Return: 0
+ */
 void print_alphabet_x10(void)
 {
 	int i = 0;
