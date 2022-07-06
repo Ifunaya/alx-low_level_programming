177405a5897eaa6af41162bd81f6857449ef3e4d
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-06 16:44:37 -0700

    multiplication table fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,26 +14,26 @@ void times_table(void)
 	{
 		for (column = 0; column <= 9; column++)
 		{
-		product = (row * column);
-
-		if (column == 0)
-		{
-			_putchar('0' + product);
+			product = (row * column);
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
 		}
-		else if (product <= 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + product);
-		}
-		else if (product > 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar('0' + (product / 10));
-			_putchar('0' + (product % 10));
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
