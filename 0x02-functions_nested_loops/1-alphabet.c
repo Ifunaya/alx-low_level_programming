646527be1b46f72bd48130b773d8a05a48f7576c
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-06 15:03:51 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - prints out the alphabet in lower case
+ *   * Return: 0
+ *    */
+void print_alphabet(void)
+{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+}
