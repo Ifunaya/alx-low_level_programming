ff765cffe5a46627c26cab1cf8255dd4f05eaa90
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-06 15:22:25 -0700

    is slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: character to check
+ * Return: 1 if c is lowercase , 0 if it's uppercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
