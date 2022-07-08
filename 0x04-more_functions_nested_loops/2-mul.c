f126aad647083da214816bf569ed15143b475c02
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-07 17:26:32 -0700

    2and3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: First integer number
+ * @b: Second integer number
+ *
+ * Return: The multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line
+* @i - the integer number to be printed
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
