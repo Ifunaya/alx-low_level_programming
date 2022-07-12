44ad18bad2f3b9ffecc1bc7cda53ccb06ac464c0
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-12 12:56:02 -0700

    unwise betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * - A function that takes a pointer to an int as parameter and updates the value it points to 98
+ * - A function that takes a pointer to an int
  * @n - a pointer to integer
  */
 
