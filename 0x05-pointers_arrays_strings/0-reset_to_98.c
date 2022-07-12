3aa00abf7e936f1957948dfd57f3ea52d2a003dc
Odiase Theophilus Ifunaya <odiaseifunaya@gmail.com>
2022-07-12 13:26:15 -0700

    hot fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * 0-reset_to_98 - takes a pointer to 98
+ * reset_to_98 - Takes a pointer to an int as parameter and updates
+ * the value it points to to 98
  * @n: a pointer to integer
  */
 
